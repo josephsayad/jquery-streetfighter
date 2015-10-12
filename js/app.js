6a4cc46f54c6f3e81c3dec934e556e1590f6cd03
Joseph Sayad <JosephSayad95@gmail.com>
2015-10-12 09:15:03 -0400

    Inserted div.main background and developed Ryu Animations

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
   console.log("Ready!");
   $(".ryu").mouseenter(function(){
+  	console.log("Mouse over Ryu");
     $(".ryu-still").hide();
     $(".ryu-ready").show();
   })
   .mouseleave(function(){
+  	console.log("Mouse off of Ryu");
     $(".ryu-ready").hide();
     $(".ryu-still").show();
+  })
+  .mousedown(function(){
+    console.log("mousedown");
+    //play hadouken sound
+    $(".ryu-ready").hide();
+    $(".ryu-throwing").show();
+    $('.hadouken').show();
+    //animate hadouken 
+  })
+  .mouseup (function(){
+    console.log("mouseup");
+    $(".ryu-throwing").hide();
+    $(".ryu-ready").show();
   });
 });
