39ed78e5b1856ac52179d12e5a20ff23041faffb
Joseph Sayad <JosephSayad95@gmail.com>
2015-10-12 10:15:18 -0400

    Hadouken animation implemented

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
 $(document).ready(function(){
-  console.log("Ready!");
-  $(".ryu").mouseenter(function(){
-  	console.log("Mouse over Ryu");
-    $(".ryu-still").hide();
-    $(".ryu-ready").show();
+  console.log('Ready!');
+  $('.ryu').mouseenter(function(){
+  	console.log('mouseenter');
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
   })
   .mouseleave(function(){
-  	console.log("Mouse off of Ryu");
-    $(".ryu-ready").hide();
-    $(".ryu-still").show();
+  	console.log('mouseleave');
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
   })
   .mousedown(function(){
-    console.log("mousedown");
+    console.log('mousedown');
     //play hadouken sound
-    $(".ryu-ready").hide();
-    $(".ryu-throwing").show();
-    $('.hadouken').show();
-    //animate hadouken 
-  })
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show().animate(
+    {'left': '1040px'},
+    600,
+    function() {
+      $(this).hide();
+      $(this).css('left', '619px');
+    })  
+  }) 
   .mouseup (function(){
-    console.log("mouseup");
-    $(".ryu-throwing").hide();
-    $(".ryu-ready").show();
+    console.log('mouseup');
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
