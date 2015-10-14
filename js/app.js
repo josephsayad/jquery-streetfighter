d934d0f82f615a0cfa74a87bdbaa908b938f840d
Joseph Sayad <JosephSayad95@gmail.com>
2015-10-13 21:51:52 -0400

    Implemented keydown and keyup methods: Ryu cool Pose

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
+
   console.log('Ready!');
+
+  $(document).keydown(function(event){
+    if (event.which == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+      $('.ryu-ready').hide();
+    }
+  })
+  .keyup(function(event){
+    if (event.which == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+
   $('.ryu').mouseenter(function(){
   	console.log('mouseenter');
+    //if (x is being pressed) {
+    // $('.ryu-ready.hide()')
+    //} else
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
@@ -14,6 +33,7 @@ $(document).ready(function(){
     console.log('mousedown');
     playHadouken();
     $('.ryu-ready').hide();
+    $('.instructions').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
     {'left': '1040px'},
@@ -27,6 +47,7 @@ $(document).ready(function(){
     console.log('mouseup');
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
+    $('.instructions').fadeIn('slow');
   });
 });
 
