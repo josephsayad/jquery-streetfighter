430aa291dacfb6295c988e5307d42132ef41c932
Joseph Sayad <JosephSayad95@gmail.com>
2015-10-23 15:48:49 -0400

    Bug fix

--- js/app.js
+++ js/app.js
@@ -10,12 +10,15 @@ $(document).ready(function() {
    * the context of the cursor within the environment 
    */
   var ryuStatus = 'still';
+  var coolStatus = 'notCool';
+  var ryu = $('.ryu');
   var ryuStill = $('.ryu-still');
   var ryuReady = $('.ryu-ready')
   var ryuThrowing = $('.ryu-throwing');
-  var ryuCool = $('.ryu-cool');
   var hadouken = $('.hadouken');
+  var ryuCool = $('.ryu-cool');
   var DEBUG = false;
+  var coolSound = false;
 
   /* Switching the value of DEBUG will fire logs [accounts]
    * of the code being executed: useful for debugging
@@ -26,7 +29,7 @@ $(document).ready(function() {
   $('.sf-logo').delay(1000).fadeIn('600').delay(4000).fadeOut('slow');
   $('.jQuery-credits').delay(1000).fadeOut('600').delay(5000).fadeIn('slow').delay(3000).fadeOut('slow');
   if(DEBUG) console.log('Enter: Ryu & Instructions');
-  $('.ryu').delay(5500).fadeOut('600').delay(5000).fadeIn('600');
+  ryu.delay(5500).fadeOut('600').delay(5000).fadeIn('600');
   $('.instructions').delay(5500).fadeOut('600').delay(5000).fadeIn('600');
 
   /* keydown and keyup event-handlers are invoked by document
@@ -37,22 +40,24 @@ $(document).ready(function() {
   $(document).keydown(function(event) {
     if (event.which == 88) {
       if(DEBUG) console.log('Keydown');
-      // ryuStatus = 'cool';
-      pauseOpening();
-      ryuStill.hide();
+      coolStatus = 'cool';
+      // playCoolMusic();
       ryuReady.hide();
       ryuThrowing.hide();
       hadouken.hide();
+      ryuStill.hide();
       ryuCool.show();
     }
   })
   .keyup(function(event) {
     if (event.which == 88) {
       if(DEBUG) console.log('Keyup');
-      ryuCool.hide();
+      coolStatus = 'notCool';
       ryuReady.hide();
       ryuThrowing.hide();
       hadouken.hide();
+      ryuStill.hide();
+      ryuCool.hide();
       //If the cursor is in div.ryu after the user releases the 
       //"x" key, then ryu-ready-position.gif will be shown
       if (ryuStatus == 'ready') {
@@ -66,17 +71,27 @@ $(document).ready(function() {
     }
   });
 
-  $('.ryu').mouseenter(function() {
+  ryu.mouseenter(function() {
+    if(DEBUG) console.log('mouseenter');
     ryuStatus = 'ready';
-  	if(DEBUG) console.log('mouseenter');
     ryuStill.hide();
-    ryuReady.show();
+    if (coolStatus == 'cool') {
+      ryuReady.hide();
+    }
+    else if (coolStatus == 'notCool'){
+      ryuReady.show();
+    }
   })
   .mouseleave(function() {
     ryuStatus = 'still';
-  	if(DEBUG) console.log('mouseleave');
+    if(DEBUG) console.log('mouseleave');
     ryuReady.hide();
-    ryuStill.show();
+    if (coolStatus == 'cool') {
+      ryuStill.hide();
+    }
+    else if (coolStatus == 'notCool') {
+      ryuStill.show();
+    }
   })
   .mousedown(function() {
     if(DEBUG) console.log('mousedown');
@@ -90,8 +105,7 @@ $(document).ready(function() {
       function() {
         $(this).hide();
         $(this).css('left', '600px');
-        //Hadouken.gif will be moved back to it's original 
-        //location
+        //Hadouken.gif will be moved back to it's original location
       }
     )  
   }) 
@@ -126,9 +140,13 @@ function pauseOpening () {
 }
 
 function playCoolMusic () {
-  $('#cool-music')[0].volume = 0.5;
-  $('#cool-music')[0].load();
-  $('#cool-music')[0].play();
+  coolSound = true;
+  if (coolSound) {
+    pauseOpening();
+    $('#cool-music')[0].volume = 0.5;
+    $('#cool-music')[0].load();
+    $('#cool-music')[0].play();
+  }
 }
 
 function pauseCoolMusic () {
